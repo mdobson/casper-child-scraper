80643ebc55b4d63d2006b1517f3c1fc5f4dfc23e
Matt Dobson <matt.dobson@paywithglyph.com>
2013-02-28 23:12:04 -0500

    Fixed the formatting of the code because it looked like hot hot garbage.

--- child_proc.js
+++ child_proc.js
@@ -1,51 +1,38 @@
- var spawn = require('child_process').spawn;
- var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+	exec = require('child_process').exec,
+	http = require('http'),
 
-var http = require('http');
-var qs = require('querystring');
 http.createServer(function (req, res) {
-    if (req.method == 'POST') {
-        var body = '';
-        req.on('data', function (data) {
-            body += data;
-        });
-        req.on('end', function () {
+	if (req.method == 'POST') {
+		var body = '';
 
-            //var POST = qs.parse(body);
-            var POST = JSON.parse(body);
-            // use POST
-            var args = ['scraper.js', '--url="'+POST.url+'"', '--selector="'+POST.selector+'"'];
-            //console.log(POST.props);
-            for (var i = 0; i < POST.props.length; i++) {
-            	var htmlProp = POST.props[i];
-            	args.push('"'+htmlProp+'"');
-            }
-            var argsString = "casperjs";
-            for (var i = 0; i < args.length; i++) {
-            	argsString += " "+args[i];
-            }
-            console.log(argsString);
+		req.on('data', function (data) {
+			body += data;
+		});
 
-
-            exec( argsString ,
-			  function (error, stdout, stderr) {
-			    
-			    if (error !== null) {
-			      console.log('exec error: ' + error);
-			    }
-			    else{
-			    	res.writeHead(200, {'Content-Type': 'application/json'});
-			    	var resp = JSON.parse(stdout);
-				 	res.end(JSON.stringify(resp));
-			    }
+		req.on('end', function () {
+			var POST = JSON.parse(body);
+			var args = ['scraper.js', '--url="'+POST.url+'"', '--selector="'+POST.selector+'"'];
+			for (var i = 0; i < POST.props.length; i++) {
+			var htmlProp = POST.props[i];
+				args.push('"'+htmlProp+'"');
+			}
+			var argsString = "casperjs";
+			for (var i = 0; i < args.length; i++) {
+				argsString += " "+args[i];
+			}
+			exec( argsString , function (error, stdout, stderr) {
+				if (error !== null) {
+					console.log('exec error: ' + error);
+				}
+				else {
+					res.writeHead(200, {'Content-Type': 'application/json'});
+					var resp = JSON.parse(stdout);
+					res.end(JSON.stringify(resp));
+				}
 			});
-   //          scraper  = spawn('casperjs', argsString);
-			// scraper.stdout.on('data', function (data) {
-		 //    	console.log('stdout: ' + data);
-		 //    	res.writeHead(200, {'Content-Type': 'application/json'});
-			// 	res.end(JSON.stringify(data));
-			// });
-        });
-    }
+		});
+	}
 }).listen(1337, '127.0.0.1');
+
 console.log('Server running at http://127.0.0.1:1337/');
